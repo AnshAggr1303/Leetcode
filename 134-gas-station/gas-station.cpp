d853ea8f835aac0764d1635215bc388d6696918b
Ansh Agrawal <103252458+AnshAggr1303@users.noreply.github.com>
2026-08-21 14:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (26.22%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalkamai = accumulate(begin(gas),end(gas),0);
+        int totalkharch = accumulate(begin(cost),end(cost),0);
+        if(totalkamai<totalkharch){
+            return -1;
+        }
+        int result_index=0;
+        int total=0;
+        for(int i=0;i<gas.size();i++){
+            total+=gas[i]-cost[i];
+            if(total<0){
+                result_index=i+1;
+                total=0;
+            }
+        }
+        return result_index;
+    }
+};
